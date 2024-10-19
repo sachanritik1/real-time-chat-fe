805141d49218a487e2749c233e82a4602778b486
Ritik Sachan <sachanritik1@gmail.com>
2024-10-19 13:27:08 +0530

    Refactor UserBox component to use user's id instead of userId

--- src/components/UserBox.tsx
+++ src/components/UserBox.tsx
@@ -15,7 +15,7 @@ const UserBox = () => {
       />
 
       <div className="text-sm font-semibold mt-2">{user?.name}</div>
-      <div className="text-xs text-gray-500">@{user?.userId}</div>
+      <div className="text-xs text-gray-500">@{user?.id}</div>
       {/* <div className="flex flex-row items-center mt-3">
         <div className="flex flex-col justify-center h-4 w-8 bg-indigo-500 rounded-full">
           <div className="h-3 w-3 bg-white rounded-full self-end mr-1"></div>
