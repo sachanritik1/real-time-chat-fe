545cf393f7b17dfc9f67579d290b53c8b87df992
Ritik Sachan <sachanritik1@gmail.com>
2024-10-19 20:29:23 +0530

    Refactor package dependencies to include @tanstack/react-query

--- src/app/SignIn.tsx
+++ src/app/SignIn.tsx
@@ -4,6 +4,7 @@ import { useRef } from "react";
 import { useRecoilState } from "recoil";
 import { userAtom } from "../store/store";
 import { useRouter } from "next/navigation";
+import { useMutation } from "@tanstack/react-query";
 
 const SignIn = () => {
   const userIdRef = useRef<HTMLInputElement>(null);
@@ -12,20 +13,36 @@ const SignIn = () => {
 
   const router = useRouter();
 
+  const {
+    mutate: login,
+    error,
+    isLoading,
+  } = useMutation({
+    mutationFn: async (user: { name: string; id: string }) => {
+      const response = await fetch(
+        process.env.NEXT_PUBLIC_BASE_URL + "/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(user),
+        }
+      );
+      const json = await response.json();
+      return json;
+    },
+    onSuccess: (data) => {
+      setUser(data?.user);
+    },
+  });
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const id = userIdRef.current?.value;
     const name = nameRef.current?.value;
-    const response = await fetch(process.env.NEXT_PUBLIC_BASE_URL + "/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name, id }),
-    });
-
-    const json = await response.json();
-    setUser(json?.user);
+    if (!id || !name) return;
+    login({ name, id });
   };
 
   if (user) {
@@ -52,6 +69,7 @@ const SignIn = () => {
           <button
             className="px-4 py-1 rounded-md text-white bg-blue-500 hover:bg-blue-600"
             type="submit"
+            disabled={isLoading}
           >
             Sign In
           </button>
--- src/context/recoil.context.tsx
+++ src/context/recoil.context.tsx
@@ -1,10 +1,17 @@
 "use client";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import { RecoilRoot } from "recoil";
 
+const queryClient = new QueryClient();
+
 function RecoilRootWrapper({ children }: { children: React.ReactNode }) {
-  return <RecoilRoot>{children}</RecoilRoot>;
+  return (
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </RecoilRoot>
+  );
 }
 
 export default RecoilRootWrapper;
